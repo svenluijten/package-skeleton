c5331f02ac21ae7631dfb301df8a3b49e911718f
Sven Luijten <svenluijten@users.noreply.github.com>
2018-10-02 22:11:58 +0200

    use phpunit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,10 @@
 
 namespace Sven\:namespace\Tests;
 
+use PhpUnit\Framework\TestCase;
 use Sven\:namespace\ServiceProvider;
-use GrahamCampbell\TestBench\AbstractPackageTestCase;
 
-abstract class TestCase extends AbstractPackageTestCase
+abstract class TestCase extends BaseTestCase
 {
-    /**
-     * Get the service provider class.
-     *
-     * @param \Illuminate\Contracts\Foundation\Application $app
-     * @return string
-     */
-    protected function getServiceProviderClass($app)
-    {
-        return ServiceProvider::class;
-    }
-
-    /**
-     * Set up the testing suite.
-     */
-    public function setUp()
-    {
-        //
-    }
-
-    /**
-     * Tear down the testing suite.
-     */
-    public function tearDown()
-    {
-        //
-    }
+    //
 }
